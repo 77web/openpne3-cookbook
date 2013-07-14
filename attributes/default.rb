aab7114580525b964a98e059de0800cda3558002
77web <info@77-web.com>
2013-07-14 12:51:47 +0900

    add default settings in attributes

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+default['openpne']['database_name'] = 'openpne'
+default['openpne']['database_user'] = 'openpne'
+default['openpne']['database_password'] = 'password'
+default['openpne']['database_sock'] = ''
+default['openpne']['database_host'] = 'localhost'
+
+default['openpne']['version'] = 'stable-3.8.x'
+
